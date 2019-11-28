857f0e11078c3705b7b07c7d4ea25361328f156b
ugo franklin <uhexos@gmail.com>
2019-11-28 14:11:39 +0000

    template react cors setup

--- old/courses/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('courses/', views.CourseList.as_view(), name='course-list'),
-    path('courses/<int:pk>/', views.CourseDetail.as_view(), name='course-detail'),
-    path('courses/<int:pk>/lessons/',views.LessonList.as_view(), name='lesson-list'),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-]
